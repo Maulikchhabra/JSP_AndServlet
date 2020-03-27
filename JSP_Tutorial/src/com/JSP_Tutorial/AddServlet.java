a9cfd72c6c116ddb6ed9133565036a5d18b3c70c
dell <dell@DESKTOP-2IRLF59>
2020-03-27 13:03:35 +0530

    Learned about different servlet calling methods

--- JSP_Tutorial/src/com/JSP_Tutorial/AddServlet.java
+++ JSP_Tutorial/src/com/JSP_Tutorial/AddServlet.java
@@ -1,5 +1,5 @@
 package com.JSP_Tutorial;
-
+import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -16,14 +16,25 @@ public class AddServlet extends HttpServlet{
 		
 		int k = i+j;
 		
+		//Doing by HttpSession
+		//HttpSession session = req.getSession();
+		//session.setAttribute("k", k);
+		
+		//Doing calling by sendRedirect theory
+	    //res.sendRedirect("sq?k="+k); //using space after = will give error  (session management or URL writing used here.)
+		
+		//Doing by cookies
+		Cookie cookie = new Cookie("k", k + ""); //Cookie is a class , making its object require two arguments 1st being the cookie name and 2nd being value but in string type.
+		res.addCookie(cookie);
+		
+		//Doing calling by request dispatcher
+		//req.setAttribute("k", k); //setting the data to request object to pass in another servlet squareservlet
+		//Request dispatcher calls one servlet from other servlet.
+		//RequestDispatcher rd = req.getRequestDispatcher("sq");
+		//rd.forward(req, res);
 		
 		
-		req.setAttribute("k", k);
-		
-		
-		RequestDispatcher rd = req.getRequestDispatcher("sq");
-		rd.forward(req, res);
-		
+		res.sendRedirect("sq");
 		
 		//PrintWriter pw = res.getWriter(); //making object of PrintWriter to print output on webpage
 		
--- JSP_Tutorial/src/com/JSP_Tutorial/SquareServlet.java
+++ JSP_Tutorial/src/com/JSP_Tutorial/SquareServlet.java
@@ -10,12 +10,31 @@ public class SquareServlet extends HttpServlet {
     
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
     
-		int k = (int)req.getAttribute("k");
+		int k=0;
+		
+		//Doing by cookies
+		Cookie cookies[] = req.getCookies(); //getting cookies from a client
+		for(Cookie c : cookies){
+			if(c.getName().equals("k")){
+				k = Integer.parseInt(c.getValue());
+			}
+		}
+		
+		
+		//Doing calling by request dispatcher
+		//int k = Integer.parseInt(req.getParameter("k"));
+		
+		//Doing by HttpSession 
+		//HttpSession session = req.getSession();
+		//int k = (int)session.getAttribute("k");
+		//We can remove value from attribute by using session.remove("k")
 		
 		k = k*k;
 		
-        PrintWriter pw = res.getWriter();
+       PrintWriter pw = res.getWriter();
         pw.println("Ressult is: "+k);
+		
+		//System.out.println("sq called!");  /debugging
     }
 	
 	
